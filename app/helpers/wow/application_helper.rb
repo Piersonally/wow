96ac33c431a197920148654c24b26909286e6fc2
Sam Pierson <sam@sampierson.com>
2014-05-04 22:19:25 -0700

    Display prices in g,s,c

--- app/helpers/wow/application_helper.rb
+++ app/helpers/wow/application_helper.rb
@@ -1,4 +1,11 @@
 module Wow
   module ApplicationHelper
+
+    def wow_price(price)
+      copper = price % 100
+      silver = price / 100 % 100
+      gold = price / 10000
+      "#{gold}g #{silver}s #{copper}c"
+    end
   end
 end
