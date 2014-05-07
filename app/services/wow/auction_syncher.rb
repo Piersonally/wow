96f7d4adef7ff2452ebfd9eac96b09bde2e55f26
Sam Pierson <sam@sampierson.com>
2014-05-06 18:22:07 -0700

    Display duration of AuctionSyncher run

--- app/services/wow/auction_syncher.rb
+++ app/services/wow/auction_syncher.rb
@@ -51,23 +51,37 @@ module Wow
       if auctions_file.last_modified_at.to_i == @realm.last_synced_at.to_i
         Rails.logger.info "AuctionSyncher: skipping #{@realm.name}, auctions file update time is still #{@realm.last_synced_at.to_i}"
       else
-        Rails.logger.info "AuctionSyncher: starting synch of #{@realm.name} #{auctions_file.last_modified_at.to_i}"
         retrieve_and_import_auctions auctions_file
-        Rails.logger.info "AuctionSyncher: completed synch of #{@realm.name} #{auctions_file.last_modified_at.to_i} at #{@realm.last_checked_at}"
       end
     end
 
     private
 
     def retrieve_and_import_auctions(auctions_file)
+      log_start auctions_file
       auctions_data = auctions_file.retrieve_auction_data
       Wow::Auction.transaction do
         import_auctions auctions_data
         @realm.update last_synced_at: auctions_file.last_modified_at
       end
+      log_end auctions_file
+    end
+
+    def log_start(auctions_file)
+      @start_time = Time.now
+      Rails.logger.info "AuctionSyncher: starting synch of %s %d at %s" %
+                          [@realm.name, auctions_file.last_modified_at.to_i, start_time]
+    end
+
+    def log_end(auctions_file)
+      elapsed_time = Time.now - @start_time
+      Rails.logger.info "AuctionSyncher: completed synch of %s %d in %.1fs" %
+                          [@realm.name, auctions_file.last_modified_at.to_i, elapsed_time]
     end
 
     def import_auctions(auctions_data)
+      @new_auctions_count = 0
+      @snapshots_created_count = 0
       @houses_to_import.each do |house|
         import_auctions_for_house house, auctions_data[house]['auctions']
       end
@@ -84,6 +98,7 @@ module Wow
     end
     
     def create_new_auction(house, auction_data)
+      @new_auctions_count += 1
       @realm.auctions.create!(
         auction_house: house,
         auc:           auction_data['auc'],
@@ -98,6 +113,7 @@ module Wow
     end
 
     def create_snapshot_of_auction(auction, auction_data)
+      @snapshots_created_count += 1
       auction.snapshots.create!(
         bid:       auction_data['bid'],
         time_left: auction_data['timeLeft']
