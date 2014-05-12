6c543a557cb19178c13238a37b01dcd26f6e0f37
Sam Pierson <sam@sampierson.com>
2014-05-12 08:45:50 -0700

    Start a wow home page for stats

---
+++ app/controllers/wow/home_controller.rb
@@ -0,0 +1,5 @@
+class Wow::HomeController < ApplicationController
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :wow do
+    get '/' => 'home#index'
     resources :auctions
     resources :realms
   end
