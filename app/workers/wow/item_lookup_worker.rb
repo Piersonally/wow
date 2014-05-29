205f762f21cade9fb8f0d72daf1e58e854f5b966
Sam Pierson <sam@sampierson.com>
2014-05-28 19:56:52 -0700

    Remove limit(5) in ItemLookupWorker

--- app/workers/wow/item_lookup_worker.rb
+++ app/workers/wow/item_lookup_worker.rb
@@ -3,15 +3,21 @@ module Wow
     include Sidekiq::Worker
 
     def perform
-      in_progress_claims_without_items.each do |claim|
-        ItemFetcher.new(claim.blizz_item_id).fetch
+      @items_created_count = 0
+      blizz_item_ids_for_in_progress_auctions.each do |blizz_item_id|
+        ItemFetcher.new(blizz_item_id).fetch
+        @items_created_count += 1
       end
+      Rails.logger.info "ItemLookupWorker: created %d items" % [@items_created_count]
     end
 
     private
 
-    def in_progress_claims_without_items
-      Wow::Auction.where status: 'in_progress', item_id: nil
+    def blizz_item_ids_for_in_progress_auctions
+      Wow::Auction.connection.select_values(
+        "SELECT DISTINCT(blizz_item_id) FROM wow_auctions
+        WHERE status = 'in_progress' AND item_id IS NULL"
+      )
     end
   end
 end
