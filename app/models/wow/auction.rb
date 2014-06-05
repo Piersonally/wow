685943bd2e462547d2ff2a2e0cbe37f62fa76a82
Sam Pierson <sam@sampierson.com>
2014-06-05 08:32:30 -0700

    Auction :dependent => :delete_alls Snapshots

--- app/models/wow/auction.rb
+++ app/models/wow/auction.rb
@@ -3,7 +3,9 @@ class Wow::Auction < ActiveRecord::Base
 
   belongs_to :realm
   belongs_to :item, class_name: 'Wow::Item'
-  has_many :snapshots, class_name: 'Wow::AuctionSnapshot'
+
+  before_destroy :clear_last_snapshot # See comment in method
+  has_many :snapshots, class_name: 'Wow::AuctionSnapshot', dependent: :delete_all
   belongs_to :last_snapshot, class_name: 'Wow::AuctionSnapshot'
 
   validates :realm_id, :auction_house, presence: true
@@ -16,5 +18,14 @@ class Wow::Auction < ActiveRecord::Base
 
   def item_name
     item_id ? item.name : blizz_item_id
+  end
+
+  private
+
+  def clear_last_snapshot
+    # Must be called BEFORE has_many :snapshots, dependent: :delete_all
+    # as :dependent is also implemented as a before_destroy callback and
+    # will therefore get executed first.
+    update_column :last_snapshot_id, nil
   end
 end
