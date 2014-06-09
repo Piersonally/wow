a4178399bce011c01460f247ad154fe5fb96ed5e
Sam Pierson <sam@sampierson.com>
2014-06-09 08:28:23 -0700

    Display money using the colored dots

--- app/helpers/wow/application_helper.rb
+++ app/helpers/wow/application_helper.rb
@@ -5,7 +5,10 @@ module Wow
       copper = price % 100
       silver = price / 100 % 100
       gold = price / 10000
-      "#{gold}g #{silver}s #{copper}c"
+      content = content_tag(:span, copper.to_s, class: 'moneycopper')
+      content = content_tag(:span, silver.to_s, class: 'moneysilver') + content if silver > 0
+      content = content_tag(:span, gold.to_s, class: 'moneygold') + content if gold > 0
+      content
     end
 
     def auctions_index_tabs_data
