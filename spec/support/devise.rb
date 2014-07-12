e14b8afd3104fa06403817eb5182f1807c3914c3
Sam Pierson <sam@sampierson.com>
2014-07-12 07:55:22 -0700

    Add Devise to dummy app
    Note: added devise gem to Gemfile instead of wow.gemspec
    because the latter requires that you then require the gem in engine.rb
    which we don't want.  We don't care where the User model comes from.

---
+++ spec/support/devise.rb
@@ -0,0 +1,9 @@
+RSpec.configure do |config|
+  config.include Devise::TestHelpers, :type => :controller
+
+  config.before(type: :controller) do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+  end
+end
+
+
